0593642ff5b3ccde10a2462ed52593e14a6bd0cd
Saharat <saharat18667@gmail.com>
2025-12-09 17:14:53 +0700

    "lab6_1.cpp completed"

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,26 @@
 using namespace std;
 
 int main(){
+    
+    int a = 0;
+    int even = 0;
+    int odd = 0;
+    
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+        cin >> a;
+        while(a != 0){
+            if(a%2 == 0){
+                even++ ;
+            }
+            else{
+                odd++ ;
+            }
+
+            cout << "Enter an integer: ";
+            cin >> a;
+        }
+        
+    cout << "#Even numbers = " << even << "\n";
+    cout << "#Odd numbers = " << odd ;
     return 0;
 }
